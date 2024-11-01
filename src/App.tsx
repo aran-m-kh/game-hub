4ea878d7bf9621315e853e18194a251894435ae1
Aran.M.Kh <mohamad.khoramdel.mk@gmail.com>
2024-11-01 13:05:31 +0100

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from '@/components/ui/button'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
-    return <Button>Click me</Button>
+    return (
+        <Grid
+            templateAreas={{
+                base: ` "nav" "main"`,
+                lg: ` "nav nav" "aside main"`, //more than 1024px
+            }}
+        >
+            <GridItem area="nav" bg={'red.300'}>
+                nav
+            </GridItem>
+
+            <GridItem hideBelow={'lg'} area="aside" bg={'blue.300'}>
+                aside
+            </GridItem>
+
+            <GridItem area="main" bg={'green.300'}>
+                main
+            </GridItem>
+        </Grid>
+    )
 }
 
 export default App
